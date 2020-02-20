d5df29881ed27acf005e85e8dbbcb54e253d97ac
Hardik Furia <hardik.furia@sonos.com>
2020-02-20 13:56:53 -0800

    General All DAG integrity and validation

---
+++ testing/dag_validation.py
@@ -0,0 +1,32 @@
+import unittest
+from airflow.models import DagBag
+
+class GeneralDagIntegrity(unittest.TestCase):
+    LOAD_SECOND_THRESHOLD = 2
+    def setUp(self):
+        self.dagbag = DagBag()
+        self.emails='airflow@example.com'
+
+    def test_import_dags(self):
+        self.assertFalse(
+            len(self.dagbag.import_errors),
+            'DAG import failures. Errors: {}'.format(
+                self.dagbag.import_errors
+            )
+        )
+
+    def test_alert_email_present(self):
+        for dag_id, dag in self.dagbag.dags.items():
+            emails = dag.default_args.get('email', [])
+            email_msg = 'Alert email not set for DAG {id}'.format(id=dag_id)
+            # descp_msg='Alert description not set for DAG {id}'.format(id=dag_id)
+            self.assertIn(self.emails, emails, email_msg)
+
+    def test_description_present(self):
+        for dag_id, dag in self.dagbag.dags.items():
+
+            descp_msg = 'Alert description not set for DAG {id}'.format(id=dag_id)
+            self.assertNotEqual(dag.description, '', descp_msg)
+
+suite = unittest.TestLoader().loadTestsFromTestCase(GeneralDagIntegrity)
+unittest.TextTestRunner(verbosity=1).run(suite)
