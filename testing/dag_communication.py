c3e9549cefc1946ea416a78fa6ce63a287ab6ad6
Hardik Furia <hardik.furia@sonos.com>
2020-02-25 10:26:48 -0800

    Fixed Parser Script

--- testing/dag_communication.py
+++ testing/dag_communication.py
@@ -16,15 +16,11 @@ class TestDagCommiunication(unittest.TestCase):
         dag = self.dagbag.get_dag(self.dag_id)
         push_to_xcoms_task = dag.get_task(self.from_task)
         pull_from_xcoms_task = dag.get_task(self.to_task1)
-
         execution_date = datetime.now()
-
         push_to_xcoms_ti = TaskInstance(task=push_to_xcoms_task, execution_date=execution_date)
         context = push_to_xcoms_ti.get_template_context()
         push_to_xcoms_task.execute(context)
-
         pull_from_xcoms_ti = TaskInstance(task=pull_from_xcoms_task, execution_date=execution_date)
-
         result = pull_from_xcoms_ti.xcom_pull(key="dummyKey")
         self.assertEqual(result, 'dummyValue')
 
@@ -32,25 +28,17 @@ class TestDagCommiunication(unittest.TestCase):
 
         dag = self.dagbag.get_dag(self.dag_id)
         push_to_xcoms_task = dag.get_task(self.from_task)
-
         execution_date = datetime.now()
-
         push_to_xcoms_ti = TaskInstance(task=push_to_xcoms_task, execution_date=execution_date)
         context = push_to_xcoms_ti.get_template_context()
         push_to_xcoms_task.execute(context)
-
         templated_xcoms_value_task = dag.get_task(self.to_task2)
         templated_xcoms_value_ti = TaskInstance(task=templated_xcoms_value_task, execution_date=execution_date)
         context = templated_xcoms_value_ti.get_template_context()
-
         bash_operator_templated_field = 'bash_command'
-
         rendered_template = templated_xcoms_value_task.render_template
-
         bash_command_value = getattr(templated_xcoms_value_task, bash_operator_templated_field)
-
         bash_command_rendered_value = rendered_template(bash_command_value,context)
-
         self.assertEqual(bash_command_rendered_value, 'echo dummyValue')
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestDagCommiunication)
--- testing/dag_definition.py
+++ testing/dag_definition.py
@@ -15,17 +15,24 @@ class TestWorkflow(unittest.TestCase):
         self.upstream_tasks=upstream_tasks
         self.downstream_tasks=downstream_tasks
 
+
     def test_task_count(self):
+        if not self.tt:
+            self.skipTest("Total tasks flag not set by users.")
         """Check task count of hello_world dag"""
         dag = self.dagbag.get_dag(self.dag_id)
         self.assertEqual(len(dag.tasks), self.tt)
 
     def test_upstream_dependency(self):
+        if not self.upstream_tasks or not self.task_id:
+            self.skipTest("Upstream Testing skipped because either the task id or the upstream tasks were not provided")
         param_task = self.dagbag.get_dag(self.dag_id).get_task(self.task_id)
         upstream_task_ids = list(map(lambda task: task.task_id, param_task.upstream_list))
         self.assertCountEqual(upstream_task_ids, self.upstream_tasks)
 
     def test_downstream_dependency(self):
+        if not self.downstream_tasks or not self.task_id:
+            self.skipTest("Downstream Testing skipped because either the task id or the downstream tasks were not provided")
         param_task = self.dagbag.get_dag(self.dag_id).get_task(self.task_id)
         downstream_task_ids = list(map(lambda task: task.task_id, param_task.downstream_list))
         self.assertCountEqual(downstream_task_ids, self.downstream_tasks)
@@ -35,9 +42,9 @@ if __name__ == '__main__':
     dagbag = DagBag()
     parser = argparse.ArgumentParser("DAG Definition testing\n")
     parser.add_argument("dag_id",help="Id of the DAG whose defn is to be tested.")
-    parser.add_argument("-tt", "--total-tasks", type=int)
-    parser.add_argument("-ut", "--upstream-tasks",action='append',help="Upstream task_ids of the task_id parsed",default=[])
-    parser.add_argument("-dt","--downstream-tasks",action='append', help="Downstream task_ids of the task_id parsed", default=[])
+    parser.add_argument("-tt", "--total-tasks", type=int, default=None)
+    parser.add_argument("-ut", "--upstream-tasks",action='append',help="Upstream task_ids of the task_id parsed",default=None)
+    parser.add_argument("-dt","--downstream-tasks",action='append', help="Downstream task_ids of the task_id parsed", default=None)
     parser.add_argument("-t","--task-id", help=" task_id to be tested. ",default='')
     args=parser.parse_args()
 
@@ -47,7 +54,8 @@ if __name__ == '__main__':
     suite = unittest.TestSuite()
 
     for test_name in test_names:
+
         suite.addTest(TestWorkflow(test_name,dagbag,args.dag_id,args.total_tasks,args.task_id,args.upstream_tasks,args.downstream_tasks))
-    result = unittest.TextTestRunner().run(suite)
+    result = unittest.TextTestRunner(verbosity=10).run(suite)
     sys.exit(not result.wasSuccessful())
 
